b10e4dcc69f1e1f3558cdf885cb3da98620a1508
zhangxiaoya <zhangxiaoya09@nudt.edu.cn>
2020-11-03 21:19:02 +0800

    update

--- plot_matrix.py
+++ plot_matrix.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 
 def generate_random_matrix(m, n):
-    random_matrix = np.random.rand(m,n)
+    random_matrix = np.random.rand(m, n)
     for i in range(m):
         for j in range(n):
             if random_matrix[i, j] >= 0.5:
@@ -13,9 +13,10 @@ def generate_random_matrix(m, n):
                 random_matrix[i, j] = 0
     return random_matrix
 
+
 def save_matrix(matrix, file_name):
-    im = Image.fromarray(matrix.astype('uint8')) # 生成 
-    im.save(file_name) # 保存
+    im = Image.fromarray(matrix.astype('uint8'))
+    im.save(file_name)
     return None
 
 if __name__ == "__main__":
